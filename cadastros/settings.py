bd328a689bb535dfa5bab14bfdbc8ebd5d2c075d
PedroAz <pdegutty@gmail.com>
2023-09-12 12:18:37 -0300

    static

--- cadastros/settings.py
+++ cadastros/settings.py
@@ -185,10 +185,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
 STATIC_ROOT = os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
