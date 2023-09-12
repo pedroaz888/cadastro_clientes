b7f190b7e67271e76110a18f0dd82e9abfba5b5e
PedroAz <pdegutty@gmail.com>
2023-09-12 12:08:23 -0300

    setting

--- clientes/urls.py
+++ clientes/urls.py
@@ -11,3 +11,6 @@ urlpatterns = [
     path('excluir_cliente/<int:id>/', views.excluir_cliente, name='excluir_cliente'),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
