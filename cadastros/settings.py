18583996aed780c2ddedd1927cae732e65078e8f
PedroAz <pdegutty@gmail.com>
2023-09-12 12:29:33 -0300

    5

--- cadastros/settings.py
+++ cadastros/settings.py
@@ -189,7 +189,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
-STATIC_ROOT = os.path.join('static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
