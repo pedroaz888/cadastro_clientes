bf40aee74cacba5271b6a20534dff6e6b65dc71b
PedroAz <pdegutty@gmail.com>
2023-09-12 12:34:25 -0300

    setting

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -5,6 +5,7 @@ from clientes import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('clientes.urls')),
--- clientes/urls.py
+++ clientes/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from clientes import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
 
